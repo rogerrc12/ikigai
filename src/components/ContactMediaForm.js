52d29962d9207d125c544e1bef875861b583e7b0
Roger Rengifo <rogerrengifo@MacBook-Pro-de-Roger.local>
2020-08-08 20:51:44 -0500

    adding form to sical media if linkedin

--- src/components/ContactMediaForm.js
+++ src/components/ContactMediaForm.js
@@ -27,7 +27,7 @@ const ContactMediaForm = () => {
     setIsLoading(true);
     const body = JSON.stringify([[values.name, values.email, values.phone]]);
     const config = { method: "POST", body, headers: { "Content-Type": "application/json" } };
-    console.log(`${process.env.GOOGLE_SHEETS_URL}?tabId=Prospectos`);
+    console.log(`https://v1.nocodeapi.com/rogerrc12/google_sheets/torcFqdyCHuSCZGm?tabId=Prospectos`);
     try {
       const response = await (await fetch(`${process.env.GOOGLE_SHEETS_URL}?tabId=Prospectos`, config)).json();
 
